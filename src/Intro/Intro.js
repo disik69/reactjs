cbcf9a7723eb7c7f51b22aba7abfd451f05c77c3
disik <disik69@rambler.ru>
2017-12-05 17:04:15 +0200

    add Intro

---
+++ src/Intro/Intro.js
@@ -0,0 +1,34 @@
+/**
+ * Created by disik on 12/5/17.
+ */
+import React, {Component} from 'react';
+import "./Intro.css";
+
+export default class Intro extends Component {
+    user = {
+        firstName: "Igor",
+        lastName: "Demchenko"
+    };
+
+    parentElement = React.createElement("p", {className: "article"}, "Article");
+
+    getIntro() {
+        return (
+                <p className="App-intro">
+                    My name is {this.user.firstName} {this.user.lastName}!
+                </p>
+
+        );
+    }
+
+    render() {
+        return (
+                <div>
+                    {this.getIntro()}
+                    {this.parentElement}
+                </div>
+
+        );
+    }
+}
+
