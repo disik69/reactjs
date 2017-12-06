110af962b4e30d67dc324cde78a49d97e8f91e46
disik <disik69@rambler.ru>
2017-12-06 14:09:12 +0200

    add clock with event handler

--- src/App.js
+++ src/App.js
@@ -5,19 +5,34 @@ import Intro from "./Intro/Intro";
 import Clock from "./Clock/Clock";
 
 class App extends Component {
-    user = {
-        firstName: "Igor",
-        lastName: "Demchenko"
+    onClickButton = (increment) => {
+        this.setState({increment: increment});
     };
+
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            increment: 10,
+            user: {
+                firstName: "Igor",
+                lastName: "Demchenko"
+            }
+        };
+    }
 
     render() {
         return (
                 <div className="App">
                     <header className="App-header">
                         <img src={logo} className="App-logo" alt="logo"/>
-                        <Clock />
+                        <Clock increment={this.state.increment} />
+                        <input type="button" value="10" onClick={(event) => this.onClickButton(10)} />
+                        <input type="button" value="20" onClick={(event) => this.onClickButton(20)} />
+                        <input type="button" value="30" onClick={(event) => this.onClickButton(30)} />
                         <h1 className="App-title">Welcome to React</h1>
-                        <Intro user={this.user} />
+                        <Intro user={this.state.user} />
                     </header>
                 </div>
         );
