297094f345efc3989a192ffeec4867753e3aab67
disik <disik69@rambler.ru>
2017-12-15 11:37:50 +0200

    move clock to component

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -5,9 +5,43 @@ import React, {Component} from "react";
 import "./Clock.css";
 
 export default class Clock extends Component {
+    constructor(props) {
+        // properties
+        super(props);
+
+        // states
+        this.state = {
+            date: new Date(),
+            counter: 0
+        };
+    }
+
+    /**
+     * Lifecycle
+     */
+    componentDidMount() {
+        this.timerId = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId)
+    }
+
+    tick() {
+        // each state was merged by set
+        this.setState({
+            date: new Date()
+        });
+
+        // arrow returns state because properties and states were changed asynchronously
+        this.setState((prevState, props) => ({
+           counter: prevState.counter + props.increment
+        }));
+    }
+
     render() {
         return (
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            <h2>It is {this.state.date.toLocaleTimeString()}. Counter is {this.state.counter}</h2>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-setInterval(function () {
-    ReactDOM.render(<App />, document.getElementById("root"));
-}, 1000);
-
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
